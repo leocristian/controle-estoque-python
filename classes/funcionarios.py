c7a019add7d47c39a9297ad5527f93473f95ce79
leocristian <leonardosclopes@gmail.com>
2020-12-06 17:59:07 -0300

    add username e senha

--- classes/funcionarios.py
+++ classes/funcionarios.py
@@ -6,11 +6,13 @@ from pessoa import *
 class Funcionario():
 
     _idFuncionario = 0
-    __slots__ = ['_pessoa']
+    __slots__ = ['_pessoa', '_username', '_senha']
 
-    def __init__(self, pessoa):
-        self._pessoa = pessoa
+    def __init__(self, _pessoa, _username, _senha):
+        self._pessoa = _pessoa
         Funcionario._idFuncionario += 1
+        self._username = _username
+        self._senha = _senha
 
     def cadastroProdutos(self, nome, descricao, preco):
         listaProdutos = []
@@ -25,10 +27,10 @@ class Funcionario():
         print("-" * 10)
 
 p1 = Pessoa("leonardo", '123', 'aaa', '234', 21, 'leo@gmail.com')
-f1 = Funcionario(p1)
+f1 = Funcionario(p1, 'leo123', '1234')
 
 p2 = Pessoa("Cristian", '321', 'bbb', '84756', 24, 'cc@gmail.com')
-f2 = Funcionario(p2)
+f2 = Funcionario(p2, 'cris123', '2345')
 
 f1.getAtributos()
 f2.getAtributos()
