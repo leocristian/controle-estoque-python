7429ad14dab76f6caaab4ff196906d60aa1db408
RuybProgramming <rubenilsondesousa@gmail.com>
2020-12-06 16:29:20 -0300

    Classe Pessoa

---
+++ classes/pessoa.py
@@ -0,0 +1,43 @@
+class Pessoa():
+
+    __slots__ = ['_nome','_cpf','_endereco','_telefone','_idade','_email']
+
+    def __init__(self,nome,cpf,endereco,telefone,idade,email):
+        self._nome = nome
+        self._cpf = cpf
+        self._endereco = endereco
+        self._idade = idade
+        self._telefone = telefone
+        self._email = email
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+    
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self._cpf = cpf
+    
+    @property
+    def endereco(self):
+        return self._endereco
+
+    @endereco.setter
+    def endereco(self, endereco):
+        self._endereco = endereco
+    
+    @property
+    def telefone(self):
+        return self._telefone
+
+    @telefone.setter
+    def telefone(self, telefone):
+        self._telefone = telefone
