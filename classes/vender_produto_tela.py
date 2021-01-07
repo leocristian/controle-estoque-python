87be4fc9c3867a78f5223485aebce4e4da63dd3d
RuybProgramming <rubenilsondesousa@gmail.com>
2021-01-07 16:17:55 -0300

    telas criadas

---
+++ classes/vender_produto_tela.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'vender_produto_tela.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(996, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(60, 140, 351, 181))
+        self.listWidget.setObjectName("listWidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(160, 110, 161, 17))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(660, 110, 161, 17))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.listWidget_2 = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget_2.setGeometry(QtCore.QRect(560, 140, 351, 181))
+        self.listWidget_2.setObjectName("listWidget_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(420, 40, 151, 17))
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(110, 360, 191, 20))
+        self.label_4.setObjectName("label_4")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(300, 360, 41, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(790, 360, 111, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(570, 360, 231, 20))
+        self.label_5.setObjectName("label_5")
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(790, 390, 41, 21))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(690, 390, 91, 20))
+        self.label_6.setObjectName("label_6")
+        self.cadastrarFuncionario = QtWidgets.QPushButton(self.centralwidget)
+        self.cadastrarFuncionario.setGeometry(QtCore.QRect(500, 480, 89, 25))
+        self.cadastrarFuncionario.setObjectName("cadastrarFuncionario")
+        self.cadastrarVoltar = QtWidgets.QPushButton(self.centralwidget)
+        self.cadastrarVoltar.setGeometry(QtCore.QRect(380, 480, 89, 25))
+        self.cadastrarVoltar.setObjectName("cadastrarVoltar")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Lista de Clientes"))
+        self.label_2.setText(_translate("MainWindow", "Lista de Produtos"))
+        self.label_3.setText(_translate("MainWindow", "Vender Produto"))
+        self.label_4.setText(_translate("MainWindow", "Selecionar Cliente pelo ID:"))
+        self.label_5.setText(_translate("MainWindow", "Selecionar Produto pelo nome:"))
+        self.label_6.setText(_translate("MainWindow", "Quantidade:"))
+        self.cadastrarFuncionario.setText(_translate("MainWindow", "Vender"))
+        self.cadastrarVoltar.setText(_translate("MainWindow", "Voltar"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
